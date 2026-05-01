182faea25bbee2d1fbd6c002b9823b4870d63d21
realJDP <realJDP@users.noreply.github.com>
2026-04-30 20:23:15 -0600

    Loan legacy transactor for XRPL transaction apply

--- src/ledger/transact/loan.rs
+++ src/ledger/transact/loan.rs
@@ -1,3 +1,4 @@
+//! xLedgRS purpose: Loan legacy transactor for XRPL transaction apply.
 use super::{legacy_path_not_supported, TxHandler, TER};
 use crate::ledger::views::ApplyView;
 use crate::transaction::ParsedTx;
